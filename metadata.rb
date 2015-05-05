8cdd8ff6f59be9dc2e5a1dbaa1fe55b55bef206b
Thomas Liebscher <thomas.liebscher@iconcifuture.com>
2015-05-05 16:46:13 +0200

    CPB-587 push cookbook version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer       "Thomas Liebscher"
 license          "Apache 2.0"
 description      "Installs phpunit, phploc, phpdpd, phpmd, phpdoc, phpcs"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "1.1.0"
 
 depends "php"
 depends "graphViz"
